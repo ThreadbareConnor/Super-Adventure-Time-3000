2970fc6c7716cde1e945528d4b6e2e96ace31a7a
ConnorThreadbare <connor@threadbaregames.com>
2014-06-30 16:30:14 -0700

    6/30 all day

--- Program.cs
+++ Program.cs
@@ -10,75 +10,95 @@ namespace Super_Adventure_Time_3000
         {
             Console.WriteLine("Please select a character 1-Barbarian 2-Knight 3-Mage");
             int characterselection = int.Parse(Console.ReadLine());
-            if(characterselection == 1)
+            if (characterselection == 1)
             {
-                    Barbarian maincharacter = new Barbarian();
-                    maincharacter.health = 10;
-                    maincharacter.str = 3;
-                    maincharacter.def = 2;
-                    maincharacter.will = 1;
-                    maincharacter.bullrush = 3;
-                    maincharacter.shootarrow = 2;
-                    maincharacter.inventory = new Inventory();
-                    maincharacter.inventory.weapon.name = "Battleaxe";
-                    maincharacter.inventory.weapon.damage = 4 + maincharacter.str;
-                    ArmorSelection();
+                Barbarian maincharacter = new Barbarian();
+                maincharacter.health = 10;
+                maincharacter.str = 3;
+                maincharacter.def = 2;
+                maincharacter.will = 1;
+                maincharacter.abilityone = 3;
+                maincharacter.abilitytwo = 2;
+                maincharacter.sweetcashmoney = 100;
+                Weapon battleaxe = new Weapon();
+                maincharacter.inventory.weapon = battleaxe;
+                maincharacter.inventory.weapon.name = "Battleaxe";
+                maincharacter.inventory.weapon.damage = 4 + maincharacter.str;
+                ArmorSelection(maincharacter);
             }
             else if (characterselection == 2)
             {
-                    Knight maincharacter = new Knight();
-                    maincharacter.health = 10;
-                    maincharacter.str = 2;
-                    maincharacter.def = 3;
-                    maincharacter.will = 2;
-                    maincharacter.slash = 2;
-                    maincharacter.shieldbash = 2;
-                    maincharacter.inventory.weapon.name = "Sword";
-                    maincharacter.inventory.weapon.damage = 3 + maincharacter.str;
-                    ArmorSelection();
+                Knight maincharacter = new Knight();
+                maincharacter.health = 10;
+                maincharacter.str = 2;
+                maincharacter.def = 3;
+                maincharacter.will = 2;
+                maincharacter.abilityone = 2;
+                maincharacter.abilitytwo = 2;
+                maincharacter.sweetcashmoney = 100;
+                Weapon sword = new Weapon();
+                maincharacter.inventory.weapon = sword;
+                maincharacter.inventory.weapon.name = "Sword";
+                maincharacter.inventory.weapon.damage = 3 + maincharacter.str;
+                ArmorSelection(maincharacter);
             }
-            else if (characterselection ==3)
+            else if (characterselection == 3)
             {
-                    Mage maincharacter = new Mage();
-                    maincharacter.health = 10;
-                    maincharacter.str = 1;
-                    maincharacter.def = 1;
-                    maincharacter.will = 4;
-                    maincharacter.fireball = 3;
-                    maincharacter.icelance = 4;
-                    maincharacter.inventory.weapon.name = "Boomstick";
-                    maincharacter.inventory.weapon.damage = 1 + maincharacter.will;
-                    ArmorSelection();
+                Mage maincharacter = new Mage();
+                maincharacter.health = 10;
+                maincharacter.str = 1;
+                maincharacter.def = 1;
+                maincharacter.will = 4;
+                maincharacter.abilityone = 3;
+                maincharacter.abilitytwo = 4;
+                maincharacter.sweetcashmoney = 100;
+                Weapon boomstick = new Weapon();
+                maincharacter.inventory.weapon = boomstick;
+                maincharacter.inventory.weapon.name = "Boomstick";
+                maincharacter.inventory.weapon.damage = 1 + maincharacter.will;
+                ArmorSelection(maincharacter);
             }
-                    
-            else 
+
+            else
             {
-                    Console.WriteLine("This is not an option");
-                    CharacterSelection();
-                    break;
+                Console.WriteLine("This is not an option");
+                CharacterSelection();
             }
         }
-        public static void ArmorSelection()
+        public static void ArmorSelection(Humanoid maincharacter)
         {
             Console.WriteLine("You arise one morning with a strong wanderlust. \n 1-Put on armor \n 2-Put on leathers \n 3-Put on robes");
             int armorselection = int.Parse(Console.ReadLine());
             switch (armorselection)
             {
                 case 1:
-                    Console.WriteLine("You put on your Knights armor");
-                    Program.FirstChoice();
+                    
+                    Armor knightsarmor = new Armor();
+                    knightsarmor.name = "Knights Armor";
+                    knightsarmor.def = 4;
+                    maincharacter.inventory.armor = knightsarmor;
+                    Console.WriteLine("You put on your {0}", maincharacter.inventory.armor.name);
+                    Program.FirstChoice(maincharacter);
                     break;
                 case 2:
-                    Console.WriteLine("You put on your Barbarian Leathers");
-                    Program.FirstChoice();
+                    Armor barbarianleaters = new Armor();
+                    barbarianleaters.name = "Barbarian Leathers";
+                    barbarianleaters.def = 3;
+                    maincharacter.inventory.armor = barbarianleaters;
+                    Console.WriteLine("You put on your {0}", maincharacter.inventory.armor.name);
+                    Program.FirstChoice(maincharacter);
                     break;
                 case 3:
-                    Console.WriteLine("You put on your Will Users Robes");
-                    Program.FirstChoice();
+                    Armor willusersrobes = new Armor();
+                    willusersrobes.name = "Will Users Robes";
+                    willusersrobes.def = 2;
+                    maincharacter.inventory.armor = willusersrobes;
+                    Console.WriteLine("You put on your {0}", maincharacter.inventory.armor.name);
+                    Program.FirstChoice(maincharacter);
                     break;
                 default:
                     Console.WriteLine("This is not an option");
-                    ArmorSelection();
+                    ArmorSelection(maincharacter);
                     break;
             }
         }
@@ -91,38 +111,36 @@ namespace Super_Adventure_Time_3000
             NewCharacter.CharacterSelection();
         }
 
-        public static void FirstChoice()
+        public static void FirstChoice(Humanoid maincharacter)
         {
-            Console.WriteLine("You walk out onto the road. To the left, the trees of the forest call to you. To the right you can smell fresh food and hear the clang of a blacksmiths hammer. \n 1-Head to the woods, who needs supplies when your\n the baddest Fuzzy Bunny on the planet. \n 2-Head to town. Even the best adventurers need food. ");
+            Console.WriteLine("You walk out onto the road. To the left, the trees of the forest call to you. \nTo the right you can smell fresh food and hear the clang of a blacksmiths hammer. \n 1-Head to the woods, who needs supplies when your\n the baddest Fuzzy Bunny on the planet. \n 2-Head to town. Even the best adventurers need food. ");
             int firstchoce = int.Parse(Console.ReadLine());
             switch (firstchoce)
             {
                 case 1:
-                    Woods.EdgeWoods();
+                    Woods.EdgeWoods(maincharacter);
                     break;
                 case 2:
-                    Town.TownSquare();
+                    Town.TownSquare(maincharacter);
                     break;
                 default:
                     Console.WriteLine("This is not an option");
-                    FirstChoice();
+                    FirstChoice(maincharacter);
                     break;
 
             }
         }
-
-
     }
     public class Woods
     {
-        public static void EdgeWoods()
+        public static void EdgeWoods(Humanoid maincharacter)
         {
             Console.WriteLine("A winding path through the trees unfolds before you. \n 1-Turn back \n 2-Continue onwards \n 3-Continue onwards but do not follow the path");
             int edgewoods = int.Parse(Console.ReadLine());
             switch (edgewoods)
             {
                 case 1:
-                    Program.FirstChoice();
+                    Program.FirstChoice(maincharacter);
                     break;
                 case 2:
                     break;
@@ -130,7 +148,7 @@ namespace Super_Adventure_Time_3000
                     break;
                 default:
                     Console.WriteLine("This is not an option");
-                    EdgeWoods();
+                    EdgeWoods(maincharacter);
                     break;
             }
         }
@@ -152,59 +170,177 @@ namespace Super_Adventure_Time_3000
     }
     public class Town
     {
-        public static void TownSquare()
+        public static void TownSquare(Humanoid maincharacter)
         {
-            Console.WriteLine("You approach the towns square. To your right there is a smithy selling solid iron swords, armor, and shields. To your left there is a small general store that seems to carry almost everything one would need to go on a lengthy adventure. \n 1-Go right \n 2-Go left");
+            Console.WriteLine("You approach the towns square. To your right there is a smithy selling solid iron swords, armor, and shields. \nTo your left there is a small general store that seems to carry almost everything \none would need to go on a lengthy adventure. \n 1-Blacksmith \n 2-General store \n 3-Go Back");
             int townchoice = int.Parse(Console.ReadLine());
             switch (townchoice)
             {
                 case 1:
-                    TownBlacksmith();
+                    TownSmith.TownBlacksmith(maincharacter);
                     break;
                 case 2:
                     break;
+                case 3:
+                    Program.FirstChoice(maincharacter);
+                    break;
                 default:
                     Console.WriteLine("This is not an option");
-                    TownSquare();
+                    TownSquare(maincharacter);
                     break;
             }
         }
-        public static void TownBlacksmith()
+       
+    }
+    public class TownSmith
+    {
+        public static void TownBlacksmith(Humanoid maincharacter)
         {
             Console.WriteLine("The smiths shop smells of burnt wood and sweat. The celing is black from \ncountless fires");
-            TownSquare();
+            Console.WriteLine("What would you like to buy? \n 1-Armor \n 2-Weapons \n 3-Go back");
+            int weaponsorarmor = int.Parse(Console.ReadLine());
+            switch (weaponsorarmor)
+            {
+                case 1:
+                    TownArmor(maincharacter);
+                    break;
+                case 2:
+                    TownWeapons(maincharacter);
+                    break;
+                case 3:
+                    Town.TownSquare(maincharacter);
+                    break;
+                default:
+                    TownBlacksmith(maincharacter);
+                    break;
+            }
         }
+        public static void TownWeapons(Humanoid maincharacter)
+        {
+            Console.WriteLine("Which weapon would you like to buy? \n 1-Bastard Sword\n 2-Bigass Axe\n 3-Flaming Sheep Launcher");
+            int armorchoice = int.Parse(Console.ReadLine());
+            switch (armorchoice)
+            {
+                case 1:
+                    Weapon bastardsword = new Weapon();
+                    bastardsword.name = "Bastard Sword";
+                    bastardsword.damage = 4;
+                    maincharacter.inventory.weapon = bastardsword;
+                    maincharacter.sweetcashmoney = maincharacter.sweetcashmoney - 25;
+                    Console.WriteLine("You pay the shopkeeper 25 SCM for the large blade. You must be compensating for something");
+                    BasicInterfaceCommands.pressonetocontinue();
+                    TownBlacksmith(maincharacter);
+                    break;
+                case 2:
+                    Weapon bigassaxe = new Weapon();
+                    bigassaxe.name = "Bigass Axe";
+                    bigassaxe.damage = 5;
+                    maincharacter.inventory.weapon = bigassaxe;
+                    maincharacter.sweetcashmoney = maincharacter.sweetcashmoney - 25;
+                    Console.WriteLine("You pay the shopkeeper 25 SCM for your shiney new {0}", maincharacter.inventory.weapon.name);
+                    BasicInterfaceCommands.pressonetocontinue();
+                    TownBlacksmith(maincharacter);
+                    break;
+                case 3:
+                    Weapon flamingsheeplauncher = new Weapon();
+                    flamingsheeplauncher.name = "Flaming Sheep Launcher";
+                    flamingsheeplauncher.damage = 2;
+                    maincharacter.inventory.weapon = flamingsheeplauncher;
+                    maincharacter.sweetcashmoney = maincharacter.sweetcashmoney - 25;
+                    Console.WriteLine("You pay the shopkeeper 25 SCM for your shiney new {0}", maincharacter.inventory.weapon.name);
+                    BasicInterfaceCommands.pressonetocontinue();
+                    TownBlacksmith(maincharacter);
+                    break;
+                default:
+                    TownWeapons(maincharacter);
+                    break;
+            }
+
+        }
+        public static void TownArmor(Humanoid maincharacter)
+        {
+            Console.WriteLine("Which set of armor would you like to buy\n 1-Lords Mail\n 2-Brutal Leathers\n 3-Silk Robes 4-Go Back");
+            int armorchoice = int.Parse(Console.ReadLine());
+            switch (armorchoice)
+            {
+                case 1:
+                    Armor lordsmail = new Armor();
+                    lordsmail.name = "Lords Mail";
+                    lordsmail.def = 6;
+                    maincharacter.inventory.armor = lordsmail;
+                    maincharacter.sweetcashmoney = maincharacter.sweetcashmoney - 25;
+                    Console.WriteLine("You pay the shopkeeper 25 SCM for your shiney new {0}", maincharacter.inventory.armor);
+                    BasicInterfaceCommands.pressonetocontinue();
+                    TownBlacksmith(maincharacter);
+                    break;
+                case 2:
+                    Armor brutalleathers = new Armor();
+                    brutalleathers.name = "Brutal Leathers";
+                    brutalleathers.def = 5;
+                    maincharacter.inventory.armor = brutalleathers;
+                    maincharacter.sweetcashmoney = maincharacter.sweetcashmoney - 25;
+                    Console.WriteLine("You pay the shopkeeper 25 SCM for your shiney new {0}", maincharacter.inventory.armor);
+                    BasicInterfaceCommands.pressonetocontinue();
+                    TownBlacksmith(maincharacter);
+                    break;
+                case 3:
+                    Armor silkrobes = new Armor();
+                    silkrobes.name = "Silk Robes";
+                    silkrobes.def = 4;
+                    maincharacter.inventory.armor = silkrobes;
+                    maincharacter.sweetcashmoney = maincharacter.sweetcashmoney - 25;
+                    Console.WriteLine("You pay the shopkeeper 25 SCM for your shiney new {0}", maincharacter.inventory.armor);
+                    BasicInterfaceCommands.pressonetocontinue();
+                    TownBlacksmith(maincharacter);
+                    break;
+                case 4:
+                    TownBlacksmith(maincharacter);
+                    break;
+                default:
+                    TownArmor(maincharacter);
+                    break;
+            }
+        }   
     }
-   
+    public class TownGerneralStore
+    {
+
+    }
     public class Barbarian : Humanoid
     {
-        public int bullrush = 2;
-        public int shootarrow = 1;
+        public int abilityone;
+        public int abilitytwo;
     }
     public class Mage : Humanoid
     {
-        public int fireball = 3;
-        public int icelance = 2;
+        public int abilityone;
+        public int abilitytwo;
     }
     public class Knight : Humanoid
     {
-        public int slash = 2;
-        public int shieldbash = 1;
+        public int abilityone;
+        public int abilitytwo;
     }
     public class Weapon
     {
-       public string name;
-       public int damage;
+        public string name;
+        public int damage;
     }
     public class Armor
     {
         public string name;
         public int def;
     }
+    public class item
+    {
+        public string name;
+        int effect;
+    }
     public class Inventory
     {
         public Weapon weapon;
         public Armor armor;
+        public item item;
 
     }
     public class Enemy : Humanoid
@@ -212,7 +348,7 @@ namespace Super_Adventure_Time_3000
         public string name;
         public int chancetodrop;
     }
-     public class Humanoid
+    public class Humanoid
     {
         public int maxhealth;
         public int health;
@@ -220,35 +356,45 @@ namespace Super_Adventure_Time_3000
         public int def;
         public int will;
         public int sweetcashmoney;
-        public Inventory inventory; 
+        public Inventory inventory = new Inventory();
     }
-        public class Battle
+    public class Battle
     {
-            public static void CallEnemy(ref object maincharacter)
-            {
-                Enemy enemy = new Enemy();
-                enemy.health = maincharacter.health - 2;
-                enemy.str = maincharacter.str - 3;
-                enemy.def = maincharacter.def -4;
-                enemy.will = maincharacter.will -2;
-                enemy.sweetcashmoney = maincharacter.sweetcashmoney / 10;
-                enemy.inventory.weapon.damage = maincharacter.inventory.weapon.damage - 2;
-            }
+        public static void CallEnemy(Humanoid maincharacter)
+        {
+            Enemy enemy = new Enemy();
+            enemy.health = maincharacter.health - 2;
+            enemy.str = maincharacter.str - 3;
+            enemy.def = maincharacter.def - 4;
+            enemy.will = maincharacter.will - 2;
+            enemy.sweetcashmoney = maincharacter.sweetcashmoney / 10;
+            enemy.inventory.weapon.damage = maincharacter.inventory.weapon.damage - 2;
+
+        }
+        
+    }
+    public class BasicInterfaceCommands
+    {
+        public static void pressonetocontinue()
+        {
+            Console.WriteLine("Enter any key to continue");
+            int didtheyhitakey = int.Parse(Console.ReadLine());
+        }
     }
 
 
 
-    
+
 }
 
 
-            
 
 
 
 
 
-       
 
-        
-    
+
+
+
+
